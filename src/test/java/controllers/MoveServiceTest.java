a2133602862c9cb25c87a18448b4ac2753a29649
Jesse Ren <jeren.neurogen@gmail.com>
2022-01-29 01:00:22 -0800

    Adding new moves with tags (relax, eod, spartan)

---
+++ src/test/java/controllers/MoveServiceTest.java
@@ -0,0 +1,17 @@
+package controllers;
+
+
+import org.testng.annotations.Test;
+
+class MoveServiceTest {
+
+    MoveService moveService = new MoveService();
+
+    MoveServiceTest() throws Exception {
+    }
+
+    @Test
+    void getAllMoves() {
+
+    }
+}
